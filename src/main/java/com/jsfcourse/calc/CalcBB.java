21aa0e109c93f24ee5cc9b6f0f8161a4addf6b27
mateu <mateu@Shadow>
2025-11-04 02:16:33 +0100

    3. Walidacja

--- src/main/java/com/jsfcourse/calc/CalcBB.java
+++ src/main/java/com/jsfcourse/calc/CalcBB.java
@@ -1,99 +1,74 @@
 package com.jsfcourse.calc;
 
-import jakarta.inject.Inject;
 import jakarta.inject.Named;
 import jakarta.enterprise.context.RequestScoped;
-import jakarta.enterprise.context.SessionScoped;
 import jakarta.faces.application.FacesMessage;
 import jakarta.faces.context.FacesContext;
 
-@Named
-@RequestScoped
-//@SessionScoped
-public class CalcBB {
-	private String kwota;
-	private String lata;
-        private String oprocentowanie;
-	private Double wynik;
+    @Named
+    @RequestScoped
+    public class CalcBB {
+        private double kwota;
+        private int lata;
+        private double oprocentowanie;
+        private Double wynik;
 
-	@Inject
-	FacesContext ctx;
+        public double getKwota() {
+            return kwota;
+        }
 
-	public String getKwota() {
-		return kwota;
-	}
+        public void setKwota(double kwota) {
+            this.kwota = kwota;
+        }
 
-	public void setKwota(String kwota) {
-		this.kwota = kwota;
-	}
+        public int getLata() {
+            return lata;
+        }
 
-	public String getLata() {
-		return lata;
-	}
+        public void setLata(int lata) {
+            this.lata = lata;
+        }
 
-	public void setLata(String lata) {
-		this.lata = lata;
-	}
-        
-        public String getOprocentowanie() {
+        public double getOprocentowanie() {
             return oprocentowanie;
         }
 
-        public void setOprocentowanie(String oprocentowanie) {
+        public void setOprocentowanie(double oprocentowanie) {
             this.oprocentowanie = oprocentowanie;
         }
-        
-	public Double getWynik() {
-		return wynik;
-	}
 
-	public void setWynik(Double wynik) {
-		this.wynik = wynik;
-	}
+        public Double getWynik() {
+            return wynik;
+        }
 
-	public boolean obliczRate() {
-                    try {
-                        double kwotaVal = Double.parseDouble(kwota);
-                        int lataVal = Integer.parseInt(lata);
-                        double oprocentowanieVal = Double.parseDouble(oprocentowanie);
+        public void setWynik(Double wynik) {
+            this.wynik = wynik;
+        }
 
-                        double miesiecznaStopa = (oprocentowanieVal / 100) / 12;
-                        int liczbaMiesiecy = lataVal * 12;
+        private void obliczRate() {
+            double miesiecznaStopa = (oprocentowanie / 100) / 12;
+            int liczbaMiesiecy = lata * 12;
 
-                        double rata = (kwotaVal * miesiecznaStopa) /
-                                (1 - Math.pow(1 + miesiecznaStopa, -liczbaMiesiecy));
+            double rata = (kwota * miesiecznaStopa) /
+                          (1 - Math.pow(1 + miesiecznaStopa, -liczbaMiesiecy));
 
-                        wynik = Math.round(rata * 100.0) / 100.0;
+            wynik = Math.round(rata * 100.0) / 100.0;
+        }
 
-                        ctx.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO,
-                                "Rata obliczona poprawnie", null));
+        public String oblicz() {
+            obliczRate();
+            return "showresult";
+        }
 
-                        return true;
+        public String oblicz_AJAX() {
+            obliczRate();
+            FacesContext.getCurrentInstance().addMessage(null,
+                    new FacesMessage(FacesMessage.SEVERITY_INFO,
+                            "Wynik: " + wynik + " PLN", null));
+            return null;
+        }
 
-                    } catch (Exception e) {
-                        ctx.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR,
-                                "Błąd: wprowadź poprawne dane liczbowe", null));
-                        return false;
-                    }
-                }
-	// Go to "showresult" if ok
-	public String oblicz() {
-		if (obliczRate()) {
-			return "showresult";
-		}
-		return null;
-	}
-
-	// Put result in messages on AJAX call
-	public String oblicz_AJAX() {
-		if (obliczRate()) {
-			ctx.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO,
-                                "Wynik: " + wynik + " PLN", null));
-		}
-		return null;
-	}
-
-	public String info() {
-		return "info";
-	}
+        public String info() {
+            return "info";
+        }
 }
